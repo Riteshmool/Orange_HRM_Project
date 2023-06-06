bce99aea1b65944735f56dcb4705861fdca6e90a
Ritesh <riteshmool83@gmail.com>
2023-06-06 14:53:49 +0530

    [Ritesh]:

--- src/main/java/orange/com/Testbase/Testbase.java
+++ src/main/java/orange/com/Testbase/Testbase.java
@@ -11,7 +11,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 
@@ -27,14 +29,14 @@ public class Testbase {
 	public LoginPage login;
 	public AdminPage admin;
 
-	@BeforeMethod
+	@BeforeClass
 	public void started() {
 		logger = Logger.getLogger("Orange HRM project");
 		PropertyConfigurator.configure("Log4j");
 		logger.info("FREAMWORK EXECUTION START");
 	}
 
-	@AfterMethod
+	@AfterClass
 	public void stopped() {
 		logger.info("FREAMWORK EXECUTION STOP");
 	}
